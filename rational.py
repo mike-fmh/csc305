0c2268db6b902c89e9d5f2f22bcc9bfbb6928fbf
Michael Miele-Herndon <mikemh@uri.edu>
2022-11-07 17:34:57 -0500

    Update rational.py

--- rational.py
+++ rational.py
@@ -1,4 +1,8 @@
+from typing import Callable
+
+
 class Rational:
+    _gcd_helper: Callable[[int, int], int] = None
     """Stores rational numbers in fraction format."""
     def __init__(self, numerator: int, denominator: int):
         """:param numerator: the top number of the rational when expressed as a fraction.
@@ -29,10 +33,23 @@ class Rational:
         else:
             return self._numerator / self._denominator
 
-
-if __name__ == '__main__':
-    rational12 = Rational(1, 2)
-    rational23 = Rational(2, 3)
-    print("Test rational12 equals another Rational(1,2):", "passed" if rational12.equals(Rational(1, 2)) else "failed")
-    print("Test rational12 not equals rational23:", "passed" if not rational12.equals(rational23) else "failed")
-    print("Test rational12 < rational23 as floats:", "passed" if rational12.as_float() < rational23.as_float() else "failed")
+    def standard_form(self) -> 'Rational':
+        """ Returns an equivalent Rational in standard form.
+        The standard, or canonical, form of a Rational has a
+        positive denominator and the numerator and denominator
+        are coprime -- their greatest common factor is 1.
+        (Thinking of the rational as a fraction, the fraction
+        cannot be reduced/simplified.)
+        :return: a new, equivalent Rational in standard form
+        """
+        # start by getting the original rational's values
+        std_numerator, std_denominator = self._numerator, self._denominator
+        if self._denominator < 0:
+            # if the denominator is negative, move the negative
+            # sign to the numerator by multiplying both by -1
+            std_numerator, std_denominator = -std_numerator, -std_denominator
+        # set the std numerators & denominators to the rational's reduced form
+        divisor = Rational._gcd_helper(std_numerator, std_denominator)
+        std_numerator = std_numerator // divisor  # // is integer division
+        std_denominator = std_denominator // divisor
+        return Rational(std_numerator, std_denominator)
